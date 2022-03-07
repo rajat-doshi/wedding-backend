b56384620502532b342dc8a2b407e9bdd9727b90
rajat-doshi <rajatdoshi11@outlook.com>
2022-03-07 22:48:21 +0530

    port

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 var cors = require("cors");
 app.use(cors());
 const formData = require("express-form-data");
-const port = 3001;
+
 var bodyParser = require("body-parser");
 app.use(bodyParser.json());
 require("./Routes/index")(app);
@@ -11,6 +11,6 @@ app.use(function (err, req, res, next) {
   console.log("This is the invalid field ->", err.field);
   next(err);
 });
-
+const port = process.env.PORT || 3001;
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
